ad1c89862a56ce2efe7ce17f71ad523692a7b1d4
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-08-08 09:50:39 -0300

    feat:  sorted items list

--- src/App.js
+++ src/App.js
@@ -95,10 +95,32 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onRemoveItems, onToggleItem }) {
+  //states
+  const [sortBy, setSortBy] = useState("input");
+
+  //derived states
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -107,6 +129,13 @@ function PackingList({ items, onRemoveItems, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
